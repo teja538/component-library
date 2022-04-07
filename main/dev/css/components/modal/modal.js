fe56455bedd1d198e8c5d35a3c24ebcc10b3386e
teja538 <saisree538@gmail.com>
2022-04-07 09:39:29 +0530

    style:PR review suggestions

--- main/dev/css/components/modal/modal.js
+++ main/dev/css/components/modal/modal.js
@@ -7,29 +7,24 @@ const modalBtn2 = document.querySelector(".modal-with-btn-btn")
 const modalWithBtn = document.querySelector(".modal-with-btn");
 const accept = document.querySelector(".accept");
 
-function displayBlock(){
-    modal.style.display = "block"; 
+function displayBlock() {
+    modal.style.display = "block";
 }
-function displayNone(){
+
+function displayNone() {
     modal.style.display = "none"
 }
 
-function displayBlock2(){
-    modal2.style.display = "block"; 
+function displayBlock2() {
+    modal2.style.display = "block";
 }
-function displayNone2(){
+
+function displayNone2() {
     modal2.style.display = "none"
 }
 
-
-modalBtn.addEventListener("click",displayBlock)
-close1.addEventListener("click",displayNone)
-modalBtn2.addEventListener("click",displayBlock2)
-close2.addEventListener("click",displayNone2)
-accept.addEventListener("click",displayNone2)
-
-
-
-
-
-
+modalBtn.addEventListener("click", displayBlock)
+close1.addEventListener("click", displayNone)
+modalBtn2.addEventListener("click", displayBlock2)
+close2.addEventListener("click", displayNone2)
+accept.addEventListener("click", displayNone2)
